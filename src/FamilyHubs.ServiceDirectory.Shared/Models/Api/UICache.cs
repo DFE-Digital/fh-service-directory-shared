6ef0eea6fa0ae6b21d11c599cbd606e3a0fcc4f1
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-08-26 07:31:27 +0100

    Added UICache

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/UICache.cs
@@ -0,0 +1,14 @@
+﻿namespace FamilyHubs.ServiceDirectory.Shared.Models.Api;
+
+public record UICache
+{
+    private UICache() { }
+
+    public UICache(string id, string value) 
+    { 
+        Id = id;
+        Value = value;
+    }
+    public string Id { get; set; } = default!;
+    public string Value { get; init; } = default!;
+}
