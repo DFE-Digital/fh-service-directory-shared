edee65ef8cd4f839e2fdc8e4bd9cd4d81befc3aa
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-02 14:00:56 +0100

    Added public Constructor

--- src/FamilyHubs.ServiceDirectory.Shared/Models/Api/UICacheDto.cs
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/UICacheDto.cs
@@ -2,6 +2,13 @@
 
 public class UICacheDto
 {
+    private UICacheDto() { }
+    public UICacheDto(string id, string value)
+    {
+        Id = id;
+        Value = value;
+    }
+
     public string Id { get; set; } = default!;
     public string Value { get; set; } = default!;
 }
