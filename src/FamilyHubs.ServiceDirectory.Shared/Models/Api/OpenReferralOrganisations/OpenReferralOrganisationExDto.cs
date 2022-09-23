c42f9d4d1800f6384745f79ece7afd5b7e4bfb9d
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-09-23 11:51:49 +0100

    Added OpenReferralOrganisationExDto

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/OpenReferralOrganisations/OpenReferralOrganisationExDto.cs
@@ -0,0 +1,28 @@
+﻿namespace FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralOrganisations;
+
+public record OpenReferralOrganisationExDto : OpenReferralOrganisationDto
+{
+    private OpenReferralOrganisationExDto() { }
+    public OpenReferralOrganisationExDto(string id = default!,
+        string organisationTypeId = default!,
+        string name = default!,
+        string? description = default!,
+        string? logo = default!,
+        string? uri = default!,
+        string? url = default!,
+        string? email = default!,
+        string? contactName = default!,
+        string? contactPhone = default!)
+        : base(id, name, description, logo, uri, url)
+    {
+        Email = email;
+        ContactName = contactName;
+        ContactPhone = contactPhone;
+        OrganisationTypeId = organisationTypeId;
+    }
+
+    public string? Email { get; set; } = default!;
+    public string? ContactName { get; set; } = default!;
+    public string? ContactPhone { get; set; } = default!;
+    public string OrganisationTypeId { get; set; } = default!;
+}
