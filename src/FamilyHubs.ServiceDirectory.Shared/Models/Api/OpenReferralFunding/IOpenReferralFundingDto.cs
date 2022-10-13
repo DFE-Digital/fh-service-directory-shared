30e8daadb8d405e623d5d8978666c9e75ba4ef88
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-10-13 15:00:51 +0100

    Added extra entities

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/OpenReferralFunding/IOpenReferralFundingDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralFunding;
+
+public interface IOpenReferralFundingDto
+{
+    public string Id { get; init; }
+    public string Source { get; init; }
+}
