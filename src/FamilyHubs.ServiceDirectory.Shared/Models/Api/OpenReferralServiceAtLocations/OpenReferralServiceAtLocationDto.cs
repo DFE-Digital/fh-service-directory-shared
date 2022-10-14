da0d8f53672f38e98ec094b67c2e13850ff9f643
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-10-14 10:34:25 +0100

    Hooked in Regular / Holiday Schedule

--- src/FamilyHubs.ServiceDirectory.Shared/Models/Api/OpenReferralServiceAtLocations/OpenReferralServiceAtLocationDto.cs
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/OpenReferralServiceAtLocations/OpenReferralServiceAtLocationDto.cs
@@ -1,4 +1,6 @@
-﻿using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralLocations;
+﻿using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralHolidaySchedule;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralLocations;
+using FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralRegularSchedule;
 
 namespace FamilyHubs.ServiceDirectory.Shared.Models.Api.OpenReferralServiceAtLocations;
 
@@ -7,19 +9,21 @@ public record OpenReferralServiceAtLocationDto : IOpenReferralServiceAtLocationD
     public OpenReferralServiceAtLocationDto() { }
     public OpenReferralServiceAtLocationDto(
         string id,
-        OpenReferralLocationDto location
-        //ICollection<OpenReferralHoliday_Schedule>? holidayScheduleCollection, ICollection<OpenReferralRegular_Schedule>? regular_schedule
+        OpenReferralLocationDto location,
+        ICollection<OpenReferralRegularScheduleDto>? regular_schedule,
+        ICollection<OpenReferralHolidayScheduleDto>? holidayScheduleCollection 
         )
     {
         Id = id;
         Location = location;
-        //HolidayScheduleCollection = holidayScheduleCollection;
-        //Regular_schedule = regular_schedule;
+        HolidayScheduleCollection = holidayScheduleCollection;
+        Regular_schedule = regular_schedule;
     }
 
     public string Id { get; set; } = default!;
     public OpenReferralLocationDto Location { get; init; } = default!;
-    //public ICollection<OpenReferralHoliday_Schedule>? HolidayScheduleCollection { get; init; }
-    //public ICollection<OpenReferralRegular_Schedule>? Regular_schedule { get; init; }
+    public ICollection<OpenReferralRegularScheduleDto>? Regular_schedule { get; init; }
+    public ICollection<OpenReferralHolidayScheduleDto>? HolidayScheduleCollection { get; init; }
+    
 }
 
