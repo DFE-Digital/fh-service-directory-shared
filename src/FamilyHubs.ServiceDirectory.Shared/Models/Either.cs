a5a3f3a0281205accb8428827958ea0f22a3702f
MartinBelton-gov <111495215+MartinBelton-gov@users.noreply.github.com>
2022-10-27 13:30:30 +0100

    Updated Referral Dto and Added Extra Models (#12)
    * Added Extra Dto's
    
    * Added ModelLinkDto
    Added Text to ReferralDto
    
    * Modified ReferralDto
    Added Multi Class Template Class
    
    * Fixed ModelLinkDto

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Either.cs
@@ -0,0 +1,10 @@
+﻿namespace FamilyHubs.ServiceDirectory.Shared.Models;
+
+//General Template to include multiple classes
+public class Either<TFirst, TSecond, TThird>
+{
+    public TFirst First { get; set; } = default!;
+    public TSecond Second { get; set; } = default!;
+    public TThird Third { get; set; } = default!;
+
+}
