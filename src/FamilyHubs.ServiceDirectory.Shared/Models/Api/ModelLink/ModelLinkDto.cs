0a81122bf64f243782ae1054422f1e7b75fccb92
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2022-10-27 12:22:41 +0100

    Fixed ModelLinkDto

--- src/FamilyHubs.ServiceDirectory.Shared/Models/Api/ModelLink/ModelLinkDto.cs
+++ src/FamilyHubs.ServiceDirectory.Shared/Models/Api/ModelLink/ModelLinkDto.cs
@@ -2,6 +2,15 @@
 
 public class ModelLinkDto : IModelLinkDto
 {
+    private ModelLinkDto() { }
+    public ModelLinkDto(string id, string linkType, string modelOneId, string modelTwoId)
+    {
+        Id = id;
+        LinkType = linkType;
+        ModelOneId = modelOneId;
+        ModelTwoId = modelTwoId;
+    }
+
     public string Id { get; set; } = default!;
     public string LinkType { get; set; } = default!;
     public string ModelOneId { get; set; } = default!;
