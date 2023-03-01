f3c617cbf452119f074d58c7cfc340e181cd135a
Phil Davies <github@phildavies.co.uk>
2023-03-01 10:40:26 +0000

    add some unit tests for GetContact

---
+++ tests/FamilyHubs.ServiceDirectory.Shared.UnitTests/Extensions/ServiceDtoExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Dto;
+using FamilyHubs.ServiceDirectory.Shared.Extensions;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectory.Shared.UnitTests.Extensions;
+
+public class ServiceDtoExtensionTests
+{
+    public ServiceDto ServiceDto { get; set; }
+
+    public ServiceDtoExtensionTests()
+    {
+        ServiceDto = new ServiceDto();
+    }
+
+    [Fact]
+    public void GetContactShouldReturnNullWhenServiceDeliveriesIsNull()
+    {
+        var result = ServiceDto.GetContact();
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetContactShouldReturnNullWhenServiceDeliveriesIsEmpty()
+    {
+        ServiceDto.ServiceDeliveries = new List<ServiceDeliveryDto>();
+        var result = ServiceDto.GetContact();
+        Assert.Null(result);
+    }
+}
