5e66d96ee67d1ad61e6b0893d0e20d20a79b3ecb
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-04-24 09:11:23 +0100

    Modified existing and Added new Referral Dto entities

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Dto/Referral/ReferrerDto.cs
@@ -0,0 +1,25 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Dto.BaseDto;
+
+namespace FamilyHubs.ServiceDirectory.Shared.Dto.Referral;
+
+public record ReferrerDto : DtoBase<long>
+{
+    public required string EmailAddress { get; set; }
+
+    public override int GetHashCode()
+    {
+        return
+           EqualityComparer<string>.Default.GetHashCode(EmailAddress) * -1521134295;
+    }
+
+    public virtual bool Equals(ReferrerDto? other)
+    {
+        if (other is null) return false;
+
+        if (ReferenceEquals(this, other))
+            return true;
+
+        return
+            EqualityComparer<string>.Default.Equals(EmailAddress, other.EmailAddress);
+    }
+}
