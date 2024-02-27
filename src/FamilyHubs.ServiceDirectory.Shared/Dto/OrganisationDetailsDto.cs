dc74ab8096588b32c7b62b3829a74d968de4b6d6
Phil Davies <github@phildavies.co.uk>
2024-02-27 11:44:49 +0000

    rename OrganisationWIthServicesDto => OrganisationDetailsDto and add locations to it

--- src/FamilyHubs.ServiceDirectory.Shared/Dto/OrganisationDetailsDto.cs
+++ src/FamilyHubs.ServiceDirectory.Shared/Dto/OrganisationDetailsDto.cs
@@ -1,6 +1,7 @@
 ﻿namespace FamilyHubs.ServiceDirectory.Shared.Dto;
 
-public record OrganisationWithServicesDto : OrganisationDto
+public record OrganisationDetailsDto : OrganisationDto
 {
     public ICollection<ServiceDto> Services { get; set; } = new List<ServiceDto>();
+    public ICollection<LocationDto> Location { get; set; } = new List<LocationDto>();
 }
