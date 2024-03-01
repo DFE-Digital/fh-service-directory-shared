fb0571930e5ed8957bfc04857a21e52a620ab36b
Phil Davies <github@phildavies.co.uk>
2024-03-01 16:04:56 +0000

    Location => Locations on org

--- src/FamilyHubs.ServiceDirectory.Shared/Dto/OrganisationDetailsDto.cs
+++ src/FamilyHubs.ServiceDirectory.Shared/Dto/OrganisationDetailsDto.cs
@@ -3,5 +3,5 @@
 public record OrganisationDetailsDto : OrganisationDto
 {
     public ICollection<ServiceDto> Services { get; set; } = new List<ServiceDto>();
-    public ICollection<LocationDto> Location { get; set; } = new List<LocationDto>();
+    public ICollection<LocationDto> Locations { get; set; } = new List<LocationDto>();
 }
