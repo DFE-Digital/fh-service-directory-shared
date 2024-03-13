17afc353550ef95c7ed413a1bea8a558d871cc7e
Phil Davies <github@phildavies.co.uk>
2024-03-13 15:20:11 +0000

    add creat/update service dto

---
+++ src/FamilyHubs.ServiceDirectory.Shared/CreateUpdateDto/ServiceDto.cs
@@ -0,0 +1,32 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Dto;
+using FamilyHubs.ServiceDirectory.Shared.Dto.BaseDto;
+using FamilyHubs.ServiceDirectory.Shared.Enums;
+
+namespace FamilyHubs.ServiceDirectory.Shared.CreateUpdateDto;
+
+public record ServiceDto : DtoBase
+{
+    public long OrganisationId { get; set; }
+    public required string ServiceOwnerReferenceId { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Fees { get; set; }
+    public string? Accreditations { get; set; }
+    public DateTime? AssuredDate { get; set; }
+    public required ServiceType ServiceType { get; set; }
+    public ServiceStatusType Status { get; set; }
+    public string? InterpretationServices { get; set; }
+    public DeliverableType DeliverableType { get; set; }
+    public double? Distance { get; set; }
+    public bool CanFamilyChooseDeliveryLocation { get; set; }
+    public ICollection<ServiceDeliveryDto> ServiceDeliveries { get; set; } = new List<ServiceDeliveryDto>();
+    public ICollection<EligibilityDto> Eligibilities { get; set; } = new List<EligibilityDto>();
+    public ICollection<FundingDto> Fundings { get; set; } = new List<FundingDto>();
+    public ICollection<CostOptionDto> CostOptions { get; set; } = new List<CostOptionDto>();
+    public ICollection<LanguageDto> Languages { get; set; } = new List<LanguageDto>();
+    public ICollection<ServiceAreaDto> ServiceAreas { get; set; } = new List<ServiceAreaDto>();
+    public ICollection<long> LocationIds { get; set; } = new List<long>();
+    public ICollection<long> TaxonomyIds { get; set; } = new List<long>();
+    public ICollection<ScheduleDto> Schedules { get; set; } = new List<ScheduleDto>();
+    public ICollection<ContactDto> Contacts { get; set; } = new List<ContactDto>();
+}
