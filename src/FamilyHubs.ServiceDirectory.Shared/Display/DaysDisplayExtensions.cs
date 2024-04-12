aa9708fb191fd9f312f8ca38bed8fc0caaa07446
Phil Davies <github@phildavies.co.uk>
2024-04-12 10:20:21 +0100

    add display code for days of the week

---
+++ src/FamilyHubs.ServiceDirectory.Shared/Display/DaysDisplayExtensions.cs
@@ -0,0 +1,17 @@
+﻿
+using FamilyHubs.ServiceDirectory.Shared.ReferenceData.ICalendar;
+
+namespace FamilyHubs.ServiceDirectory.Shared.Display;
+
+public static class DaysDisplayExtensions
+{
+    public static string GetDayNames(this IEnumerable<string>? days)
+    {
+        if (days == null || !days.Any())
+        {
+            return "None provided";
+        }
+
+        return string.Join(", ", days.Select(c => Calendar.DayCodeToName[c]));
+    }
+}
