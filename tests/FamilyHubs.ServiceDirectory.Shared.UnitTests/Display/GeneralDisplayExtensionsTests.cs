52d46a5524862654ee4f1751b2a6e0bea28e99ea
Phil Davies <github@phildavies.co.uk>
2024-05-15 13:03:54 +0100

    add GetDisplay helper that replaces empty strings with 'None Provided'

---
+++ tests/FamilyHubs.ServiceDirectory.Shared.UnitTests/Display/GeneralDisplayExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using FamilyHubs.ServiceDirectory.Shared.Display;
+using Xunit;
+
+namespace FamilyHubs.ServiceDirectory.Shared.UnitTests.Display;
+
+public class GeneralDisplayExtensionsTests
+{
+    [Theory]
+    [InlineData(null, "None provided")]
+    [InlineData("", "None provided")]
+    [InlineData("Some description", "Some description")]
+    public void GetDisplay(string? description, string expected)
+    {
+        // Act
+        var result = description.GetDisplay();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
